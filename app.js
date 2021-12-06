72bb602bfa8396394ab7d725026044e8ffdacde6
Nimrod Flaschner <nimrodifla@gmail.com>
2021-12-06 16:47:19 +0200

    design

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/favicon.ico", (req, res)=>{
+    res.sendFile(__dirname + "/favicon.ico");
+});
+
 app.listen(process.env.PORT || 80, (err)=>{
     if (err)
         throw err;
     
     console.log("started");
-})
+});
